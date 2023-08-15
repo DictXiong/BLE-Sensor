2d07a27867b94120ca58ef3ddf44b717424176bd
xiongdian.me <xiongdian.me@bytedance.com>
2023-08-15 12:41:32 +0800

    sht4x: crc use lookup table

--- hardware/kit/common/drivers/sht4x.c
+++ hardware/kit/common/drivers/sht4x.c
@@ -14,31 +14,30 @@
 #define SHT4X_CMD_MEASURE_LPM 0xE0
 #define SHT4X_CMD_READ_SERIAL 0x89
 
-static uint8_t sht4x_crc(uint16_t value, uint8_t crc);
-static uint8_t sht4x_crc(uint16_t value, uint8_t crc) {
-    uint32_t polynom = 0x988000U;
-    uint32_t msb     = 0x800000U;
-    uint32_t mask    = 0xFF8000U;
-    uint32_t result  = (uint32_t)value << 8;
-
-    while (msb != 0x80)                                                       /* check the msb */
-    {
-        if ((result & msb) != 0)                                              /* check the result */
-        {
-            result = ((result ^ polynom) & mask) | (result & (~mask));        /* get the new result */
-        }
-        msb >>= 1;                                                            /* right shift 1 */
-        mask >>= 1;                                                           /* right shift 1 */
-        polynom >>= 1;                                                        /* right shift 1 */
-    }
-    if (result == crc)                                                        /* check the result */
-    {
-        return 0;                                                             /* success return 0 */
-    }
-    else
-    {
-        return 1;                                                             /* return error */
-    }
+static bool sht4x_crc(uint16_t value, uint8_t crc);
+static uint8_t sht4x_crc_table[] = {
+  0,   49,  98,  83,  196, 245, 166, 151, 185, 136, 219, 234, 125, 76,  31,  46,
+  67,  114, 33,  16,  135, 182, 229, 212, 250, 203, 152, 169, 62,  15,  92,  109,
+  134, 183, 228, 213, 66,  115, 32,  17,  63,  14,  93,  108, 251, 202, 153, 168,
+  197, 244, 167, 150, 1,   48,  99,  82,  124, 77,  30,  47,  184, 137, 218, 235,
+  61,  12,  95,  110, 249, 200, 155, 170, 132, 181, 230, 215, 64,  113, 34,  19,
+  126, 79,  28,  45,  186, 139, 216, 233, 199, 246, 165, 148, 3,   50,  97,  80,
+  187, 138, 217, 232, 127, 78,  29,  44,  2,   51,  96,  81,  198, 247, 164, 149,
+  248, 201, 154, 171, 60,  13,  94,  111, 65,  112, 35,  18,  133, 180, 231, 214,
+  122, 75,  24,  41,  190, 143, 220, 237, 195, 242, 161, 144, 7,   54,  101, 84,
+  57,  8,   91,  106, 253, 204, 159, 174, 128, 177, 226, 211, 68,  117, 38,  23,
+  252, 205, 158, 175, 56,  9,   90,  107, 69,  116, 39,  22,  129, 176, 227, 210,
+  191, 142, 221, 236, 123, 74,  25,  40,  6,   55,  100, 85,  194, 243, 160, 145,
+  71,  118, 37,  20,  131, 178, 225, 208, 254, 207, 156, 173, 58,  11,  88,  105,
+  4,   53,  102, 87,  192, 241, 162, 147, 189, 140, 223, 238, 121, 72,  27,  42,
+  193, 240, 163, 146, 5,   52,  103, 86,  120, 73,  26,  43,  188, 141, 222, 239,
+  130, 179, 224, 209, 70,  119, 36,  21,  59,  10,  89,  104, 255, 206, 157, 172
+};
+static bool sht4x_crc(uint16_t value, uint8_t crc) {
+  uint8_t result = 0xff;
+  result = sht4x_crc_table[(uint8_t)(value >> 8) ^ result];
+  result = sht4x_crc_table[(uint8_t)(value) ^ result];
+  return result == crc;
 }
 
 int8_t sht4x_read_serial(I2C_TypeDef *i2c, uint32_t* serial) {
@@ -59,10 +58,10 @@ int8_t sht4x_read_serial(I2C_TypeDef *i2c, uint32_t* serial) {
   if (ret == i2cTransferDone) {
     uint16_t tmp;
     tmp = (i2c_read_data[0] << 8) | i2c_read_data[1];
-    if (sht4x_crc(tmp, i2c_read_data[2])) return STATUS_CRC_ERR;
+    if (!sht4x_crc(tmp, i2c_read_data[2])) return STATUS_CRC_ERR;
     serial = tmp << 16;
     tmp = (i2c_read_data[3] << 8) | i2c_read_data[4];
-    if (sht4x_crc(tmp, i2c_read_data[5])) return STATUS_CRC_ERR;
+    if (!sht4x_crc(tmp, i2c_read_data[5])) return STATUS_CRC_ERR;
     serial = serial | tmp;
   }
   return (int8_t) ret;
@@ -102,8 +101,8 @@ int8_t sht4x_read_measurements(I2C_TypeDef *i2c, uint16_t *temperature, uint16_t
   if (ret == i2cTransferDone) {
     *temperature = (i2c_read_data[0] << 8) | i2c_read_data[1];
     *humidity = (i2c_read_data[3] << 8) | i2c_read_data[4];
-    if (sht4x_crc(temperature, i2c_read_data[2])) return STATUS_CRC_ERR;
-    if (sht4x_crc(humidity, i2c_read_data[5])) return STATUS_CRC_ERR;
+    if (!sht4x_crc(temperature, i2c_read_data[2])) return STATUS_CRC_ERR;
+    if (!sht4x_crc(humidity, i2c_read_data[5])) return STATUS_CRC_ERR;
   }
 
   return (int8_t) ret;
